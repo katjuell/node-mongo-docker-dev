ffc281b0490a2f6e0d73640c4d0e24acd616b1c5
Kat Juell <kathleenjuell@gmail.com>
2019-02-13 22:01:16 +0000

    added sammy username for demo

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const MONGO_USERNAME = 'mrcat';
-const MONGO_PASSWORD = 'brow789*()+';
+const MONGO_USERNAME = 'sammy';
+const MONGO_PASSWORD = 'password';
 const MONGO_HOSTNAME = '127.0.0.1';
 const MONGO_PORT = '27017';
 const MONGO_DB = 'sharkinfo';
