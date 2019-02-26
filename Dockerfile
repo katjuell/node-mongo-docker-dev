2885191951459d35d0ed0565241df71f0e239400
Kat Juell <kathleenjuell@gmail.com>
2019-02-26 18:30:40 +0000

    initial commit

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,13 @@ RUN mkdir -p /home/node/app/node_modules && chown -R node:node /home/node/app
 
 WORKDIR /home/node/app
 
-COPY package*.json ./
+COPY app/package*.json ./
 
 USER node
 
 RUN npm install
 
-COPY --chown=node:node . .
+COPY --chown=node:node ./app .
 
 EXPOSE 8080
 
--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ const db = require('./db');
 const sharks = require('./routes/sharks');
 
 const path = __dirname + '/views/';
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -14,5 +14,5 @@ app.use(express.static(path));
 app.use('/sharks', sharks);
 
 app.listen(port, function () {
-  console.log('Example app listening on port 8080!')
+  console.log(`Example app listening on ${port}!`)
 })
